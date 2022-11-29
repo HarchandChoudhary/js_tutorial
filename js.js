c858c1461a8f85ea672ae1a7aeb6d1492a43d0a1
Harchand Choudhary <HarchandChoudhary8209@gmail.com>
2022-11-29 15:14:02 +0530

    test js

---
+++ js.js
@@ -0,0 +1,77 @@
+// Language browser m daal de gye?
+// browser m 1 console hota h jisme language ka interpriter (run code line by line do not complie all file once) run hota h
+
+// Hello word in JS
+console.log("hello world")
+
+// variables 
+a = 7
+a = "sdfsdf"
+console.log(a)
+
+// OPERATORS
+
+4 + 5
+"ABC" + 66
+"9" - 66
+a = 56
+b = 4
+console.log(a+b)
+
+"20" == 20 // true
+"20" === 20 // false
+
+a = "a"
+console.log(a+b)
+c = a + b
+console.log(c)
+
+// data type
+integer = 5656 // any numaric value
+strings = "dfjhdfgdg" // any string or anything inside quotes ' "" """ '''
+bool = true // or false
+
+// typeof to get the data type
+typeof(bool)
+
+// IF ELSE
+
+if ( "vipin" == "vipin" ){
+	console.log("yes donon string barabar h")
+}else
+{
+	console.log("nahi string barabar nahi h")
+}
+
+// Switch statment withougth break
+switch (3){
+	case 1: 
+		console.log(1)
+	case 2:
+		console.log(2)
+	case 3:
+		console.log(3)
+	case 4:
+		console.log(4)
+	case 1:
+		console.log(1)
+} // 3 4 1
+
+// Switch statment with break
+switch (3){
+        case 1:
+                console.log(1)
+        case 2:
+                console.log(2)
+        case 3:
+                console.log(3)
+		break
+        case 4:
+                console.log(4)
+        case 1:
+                console.log(1)
+} // 3
+
+
+
+ 
